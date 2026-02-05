5d846104ef6a8d72f9c2b322b754d5f77ded237d
Enzo Duré <enzod98@hotmail.com>
2026-02-05 04:06:14 -0300

    Agregado swagger

--- Currency.API/Program.cs
+++ Currency.API/Program.cs
@@ -3,12 +3,26 @@ using Currency.API.Endpoints;
 using Currency.API.Middlewares;
 using Infrastructure;
 using Infrastructure.Persistence;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TechNotes API",
+        Version = "v1",
+        Description = "API Prueba Técnica"
+    });
+});
+
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -22,7 +36,8 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();   
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
