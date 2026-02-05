dd2840ae4c9a19a3d6d60dac416aa5d6d8788403
Enzo Duré <enzod98@hotmail.com>
2026-02-05 03:58:12 -0300

    Agregado hasheo de contraseñas

--- Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Users;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Application.Users.Commands.CreateUser;
 
@@ -12,7 +14,11 @@ public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, UserR
 
     public async Task<Result<UserResponse>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
+        var inputBytes = Encoding.UTF8.GetBytes(request.Password);
+        var hashBytes = SHA256.HashData(inputBytes);
+
         var newUser = request.Adapt<User>();
+        newUser.Password = Convert.ToHexString(hashBytes);
         var user = await _userRepository.CreateUserAsync(newUser);
         return user.Adapt<UserResponse>();
     }
