3c208e7ba4dbfa29b2c0a5d0542ebc28d58aebb5
Enzo Duré <enzod98@hotmail.com>
2026-02-04 22:12:48 -0300

    Application: Agregadas abstracciónes de CQRS

---
+++ Application/Abstractions/RequestHandling/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Abstractions;
+using MediatR;
+
+namespace Application.Abstractions.RequestHandling;
+
+public interface ICommand : IRequest<Result>
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ Application/Abstractions/RequestHandling/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions;
+using MediatR;
+
+namespace Application.Abstractions.RequestHandling;
+
+public interface IQuery<IResponse> : IRequest<Result<IResponse>>
+{
+}
